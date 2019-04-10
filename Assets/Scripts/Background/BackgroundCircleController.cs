ae55b1b765df99f32c18bd9958373e081a93f2a2
Justin Roderman <jcroderman@gmail.com>
2019-04-10 10:05:55 -0500

    Initial commit: Adds background

---
+++ Assets/Scripts/Background/BackgroundCircleController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundCircleController : MonoBehaviour
+{
+    private Vector3 SCALE_VECTOR = Vector3.one / 200f;
+
+    private Color spriteColor;
+    private Image spriteImg;
+    private bool fadeIn = true;
+    private bool fadeOut = false;
+
+	void Start ()
+    {
+        spriteColor = new Color(Random.value / 4, Random.value / 4, 0.5f, 0f);
+        spriteImg = gameObject.GetComponent<Image>();
+        spriteImg.color = spriteColor;
+    }
+	
+	void Update ()
+    {
+        if(fadeIn)
+        {
+            spriteColor.a += 0.001f;
+            spriteImg.color = spriteColor;
+            if (spriteColor.a > 0.2f)
+            {
+                fadeIn = false;
+                Invoke("BeginFadeOut", 2f);
+            }
+        }
+        gameObject.transform.localScale += SCALE_VECTOR;
+        if(fadeOut)
+        {
+            spriteColor.a -= 0.001f;
+            spriteImg.color = spriteColor;
+            if (spriteColor.a <= 0)
+                Destroy(gameObject);
+        }
+	}
+
+    void BeginFadeOut()
+    {
+        fadeOut = true;
+    }
+}
---
+++ Assets/Scripts/Background/BackgroundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public GameObject backgroundCirclePrefab;
+
+	void Start ()
+    {
+        Invoke("SpawnCircle", 0f);
+	}
+	
+	void SpawnCircle ()
+    {
+        if (GameObject.FindGameObjectsWithTag("BackgroundCircle").Length < 10)
+        {
+            int width = Screen.width;
+            int height = Screen.height;
+            Instantiate(backgroundCirclePrefab, new Vector3(Random.Range(-6, 6), Random.Range(-6, 6)), Quaternion.identity, gameObject.transform);
+        }
+        Invoke("SpawnCircle", Random.Range(0f, 2f));
+    }
+}
